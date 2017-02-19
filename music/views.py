f7ee13427aed0c336ee10331e9ba75d578c34ac0
Sazib <ofsazib@gmail.com>
2017-02-19 21:31:01 +0600

    check authentication and album_logo

--- music/views.py
+++ music/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import logout
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
-from .forms import AlbumForm, Songform, UserForm
+from .forms import AlbumForm, SongForm, UserForm
 from .models import Album, Song
 
 AUDIO_FILE_TYPES = ['wav', 'mp3', 'ogg']
@@ -19,3 +19,18 @@ def create_album(request):
             album = form.save(commit=False)
             album.user = request.user
             album.album_logo = request.FILES['album_logo']
+            file_type = album.album_logo.url.split('.')[-1]
+            file_type = file_type.lower()
+            if file_type not in IMAGE_FILE_TYPES:
+                context = {
+                    'album': album,
+                    'form': form,
+                    'error_message': 'Image file must be PNG, JPG or JPEG'
+                }
+                return render(request, 'music/create_album.html', context)
+            album.save()
+            return render(request, 'music/detail.html', 'album': album)
+        context = {
+            'form': form
+        }
+        return render(request, 'music/create_album.html')
