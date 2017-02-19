87849ff90d8685ea49b311f71eb9acaaf18631c2
Sazib <ofsazib@gmail.com>
2017-02-19 23:12:32 +0600

    create song view

--- music/views.py
+++ music/views.py
@@ -29,8 +29,42 @@ def create_album(request):
                 }
                 return render(request, 'music/create_album.html', context)
             album.save()
-            return render(request, 'music/detail.html', 'album': album)
+            return render(request, 'music/detail.html', {'album': album})
         context = {
-            'form': form
+            "form": form
         }
-        return render(request, 'music/create_album.html')
+        return render(request, 'music/create_album.html', context)
+
+def create_song(request, album_id):
+    form = SongForm(request.POST or None, request.FILES or None)
+    album = get_object_or_404(Album, pk=album_id)
+    if form.is_valid():
+        album_songs = album.set_song.all()
+        for s in album_songs:
+            if s.song_title == form.cleaned_data.get("song_title"):
+                context = {
+                    'album': album,
+                    'form': form,
+                    'error_message': 'The Song Is already exist'
+                }
+                return render(request, 'music/create_song.html', context)
+            song = form.save(commit=False)
+            song.album = album
+            song.audio_file = request.FILES['audio_file']
+            file_type = song.audio_file.url.aplit('.')[-1]
+            file_type = file_type.lower()
+            if file_type not in AUDIO_FILE_TYPES:
+                context = {
+                    'alnum': album,
+                    'form': form,
+                    'error_message': 'Audio file must be WAV, MP3 or OGG'
+                }
+                return render(request, 'music/create_song.html', context)
+
+            song.save()
+            return render(request, 'music/detail.html', {'album': album})
+        context = {
+            'album': album,
+            'form': form,
+        }
+        return render(request, 'music/create_song.html', context)
