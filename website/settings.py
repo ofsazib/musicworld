4eaa682e9bb38e56bb9b0441856d463421780566
Sazib <ofsazib@gmail.com>
2017-02-21 15:14:40 +0600

    config static files

--- website/settings.py
+++ website/settings.py
@@ -125,7 +125,7 @@ MEDIA_URL = '/media/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '../music/static'),
 )
 
 DATABASES['default'] = dj_database_url.config()
