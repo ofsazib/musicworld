8ccaa89c53898207c70bf1f3ba626c3ebab97216
Sazib <ofsazib@gmail.com>
2017-02-22 00:50:33 +0600

    load media

--- website/settings.py
+++ website/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
